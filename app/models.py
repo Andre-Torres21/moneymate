cc14eb33d58c3df510ce532131e1c5208e4b05a8
André Torres <161315994+Andre-Torres21@users.noreply.github.com>
2024-07-15 10:04:49 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,39 +1,52 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Categoria(models.Model):
-    nome = models.CharField(max_length=64)
-    tipo = models.CharField(max_length=64)
+    nome = models.CharField(max_length=64, verbose_name='Nome')
+    tipo = models.CharField(max_length=64, verbose_name='Tipo')
+
+    def get_absolute_url(self):
+        return reverse('categorias', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.nome
 
 class Despesa(models.Model):
-    nome = models.CharField(max_length=64)
-    valor = models.DecimalField(max_digits=8, decimal_places=2)
-    data = models.DateField()
-    observacoes = models.TextField(blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=64, verbose_name='Nome')
+    valor = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor')
+    data = models.DateField(verbose_name='Data')
+    observacoes = models.TextField(blank=True, verbose_name='Observações')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categorias')
+
+    def get_absolute_url(self):
+        return reverse('despesas', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.nome
 
 class Entrada(models.Model):
-    fonte = models.CharField(max_length=64)
-    valor = models.DecimalField(max_digits=8, decimal_places=2)
-    data = models.DateField()
-    observacoes = models.TextField(blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fonte = models.CharField(max_length=64, verbose_name='Fonte')
+    valor = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor')
+    data = models.DateField(verbose_name='Data')
+    observacoes = models.TextField(blank=True, verbose_name='Observações')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria')
+
+    def get_absolute_url(self):
+        return reverse('entradas', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.fonte
 
 class Transacao(models.Model):
-    nome = models.CharField(max_length=64)
-    valor = models.DecimalField(max_digits=8, decimal_places=2)
-    data = models.DateField()
-    observacoes = models.TextField(blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=64, verbose_name='Nome')
+    valor = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor')
+    data = models.DateField(verbose_name='Data')
+    observacoes = models.TextField(blank=True, verbose_name='Observações')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria')
+
+    def get_absolute_url(self):
+        return reverse('transacoes', kwargs={'pk': self.pk})
 
     class Meta:
         verbose_name_plural = 'Transações'
@@ -42,10 +55,13 @@ class Transacao(models.Model):
         return self.nome
 
 class MetaFinanceira(models.Model):
-    nome = models.CharField(max_length=64)
-    valor_total = models.DecimalField(max_digits=8, decimal_places=2)
-    meta_valor_mes = models.DecimalField(max_digits=8, decimal_places=2)
-    valor_guardar_mes = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    nome = models.CharField(max_length=64, verbose_name='Nome')
+    valor_total = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor total')
+    meta_valor_mes = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Meta de valor por mês')
+    valor_guardar_mes = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, verbose_name='Valor a guardar por mês')
+
+    def get_absolute_url(self):
+        return reverse('meta_financeira', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.nome
--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,18 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('despesas/', views.despesas, name='despesas'),
+    path('despesas/add', views.DespesaCreateView.as_view(), name='add_despesa'),
+    path('entradas/', views.entradas, name='entradas'),
+    path('entradas/add', views.EntradaCreateView.as_view(), name='add_entrada'),
+    path('transacoes/', views.transacoes, name='transacoes'),
+    path('transacoes/add', views.TransacaoCreateView.as_view(), name='add_transacao'),
+    path('relatorio_mensal/', views.relatorio_mensal, name='relatorio_mensal'),
+    path('metas_financeiras/', views.metas_financeiras, name='metas_financeiras'),
+    path('metas_financeiras/add', views.MetaFinanceiraCreateView.as_view(), name='add_meta_financeira'),
+    path('metas_financeiras/add_value', views.MetaFinanceiraAddValueView.as_view(), name='add_valor_meta_financeira'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('categorias/add', views.CategoriaCreateView.as_view(), name='add_categoria'),
+]
